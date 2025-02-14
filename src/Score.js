e5a50ccae86b7e60f14322ca50ea367b0486abd9
Moaz <Moazosam11@gmail.com>
2025-02-14 20:55:02 +0200

    Calc score (#2)
    * Score Function done Part 3
    
    * Score Done

---
+++ src/Score.js
@@ -0,0 +1,18 @@
+function calculateScore() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const shouldFail = Math.random() < 0.1;
+            
+            if (shouldFail) {
+                reject(new Error("Failed to calculate score"));
+            } else {
+                const score = Math.floor(Math.random() * 100);
+                console.log("Calculated score:", score);
+                resolve(score);
+            }
+        }, 500);
+    });
+}
+export { calculateScore };
+
+// Done
